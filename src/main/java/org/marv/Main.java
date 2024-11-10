24b54e8ad0cc7cf96cd4ef08b20196a2c19fbd6f
MIANREVA2024 <miguel.reyes.vasquez@gmail.com>
2024-11-10 16:45:03 +0100

    inicio

---
+++ src/main/java/org/marv/Main.java
@@ -0,0 +1,15 @@
+package org.marv;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int edad = 40;
+        double altura =1.80;
+        String name = "Miguel";
+        System.out.println("Nombre: " + name);
+        System.out.println("Edad: " + edad);
+        System.out.println("Altura: " + altura);
+
+    }
+}
